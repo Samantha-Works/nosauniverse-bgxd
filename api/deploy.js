fd7fd83fd375c9f77b7241e2dda5bf32e44579dc
Sam <donetheaceway@gmail.com>
2025-11-27 17:08:24 -0500

    Update deploy.js

--- api/deploy.js
+++ api/deploy.js
@@ -1,1 +1,1 @@
-"https://api.github.com/repos/samanthas-projects-0e1d986a/cinemorphsrl/actions/workflows/main.yml/dispatches"
+"https://api.github.com/repos/samanthas-projects-0e1d986a/novadeploy/actions/workflows/main.yml/dispatches"
