4741a1de75d5f8c38d2c21ba4313ba994d33705a
Sam <donetheaceway@gmail.com>
2025-11-27 17:50:14 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("novaButton").onclick = async () => {
-  const res = await fetch('/api/deploy', { method: 'POST' });
-  const data = await res.json();
-  alert(JSON.stringify(data));
-};
